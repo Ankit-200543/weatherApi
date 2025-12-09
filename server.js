cb8fcb34bcb634cbe1490de5e8e4fd8d2e72a34c
Ankit-200543 <ak8946562@gmail.com>
2025-12-09 12:55:45 +0530

    addded cors

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+
 
 app.get('/', (req, res) => {
     const data = { message: 'Hello World' };
